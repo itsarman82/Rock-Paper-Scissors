70ab6b5c5b60c6a8f0803afdbcba6642756f7038
arman <armans8282@gmail.com>
2022-11-17 06:28:42 +0330

    Changing to object oriented style

---
+++ Game.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{  
+    class Game
+    {
+        public static int userInput;
+        public static int Computer;
+
+        public static void playGame()
+        {
+            int userAnswer = user(userInput);
+            int computerAnswer = computer(Computer);
+
+            resualt(userAnswer, computerAnswer);
+        }   
+        public static int user(int userInput)
+        {
+            Console.WriteLine("Please enter your choice.1 for ROCK,2 for PAPER or 3 for SCISSORS");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            return userInput;
+        }
+
+        public static int computer(int computer)
+        {
+            Random myRandomObject = new Random();
+            var randomNum = myRandomObject.Next(1, 4);
+            return randomNum;
+        }
+
+        public static void resualt(int userAnswer, int computerAnswer)
+        {
+            switch (userAnswer) 
+            {
+                case 1:
+                    if (computerAnswer == 1)
+                    {
+                        Console.WriteLine("You played: Rock");
+                        Console.WriteLine("Computer played: Rock");
+                        Console.WriteLine("its a tie");
+
+                    } else if (computerAnswer == 2) 
+                    {
+                        Console.WriteLine("You played: Rock");
+                        Console.WriteLine("Computer played: paper");
+                        Console.WriteLine("You Lost!!");
+
+                    }else if (computerAnswer == 3)
+                    {
+                        Console.WriteLine("You played: Rock");
+                        Console.WriteLine("Computer played: scissors");
+                        Console.WriteLine("You Won!!");
+                    }
+                    break;
+
+                case 2:
+                    if (computerAnswer == 1)
+                    {
+                        Console.WriteLine("You played: paper");
+                        Console.WriteLine("Computer played: Rock");
+                        Console.WriteLine("You Won!!");
+
+                    }
+                    else if (computerAnswer == 2)
+                    {
+                        Console.WriteLine("You played: paper");
+                        Console.WriteLine("Computer played: paper");
+                        Console.WriteLine("its a tie");
+
+                    }
+                    else if (computerAnswer == 3)
+                    {
+                        Console.WriteLine("You played: paper");
+                        Console.WriteLine("Computer played: scissors");
+                        Console.WriteLine("You Lost!!");
+                    }
+                    break;
+
+                case 3:
+                    if (computerAnswer == 1)
+                    {
+                        Console.WriteLine("You played: scissors");
+                        Console.WriteLine("Computer played: Rock");
+                        Console.WriteLine("You Lost!!");
+
+                    }
+                    else if (computerAnswer == 2)
+                    {
+                        Console.WriteLine("You played: scissors");
+                        Console.WriteLine("Computer played: paper");
+                        Console.WriteLine("You Won!!");
+
+                    }
+                    else if (computerAnswer == 3)
+                    {
+                        Console.WriteLine("You played: scissors");
+                        Console.WriteLine("Computer played: scissors");
+                        Console.WriteLine("its a tie");
+                    }
+                    break;
+
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,98 +4,19 @@
     {
         static void Main(string[] args)
         {
+            // this app develpped with .net core v7
+            // this app developed in Visual studio 2022 Enterprise
+            // warning : this app may not run on your enviroments except build it.
+
             Console.WriteLine("Welcome to Rock Paper Scissors Cli game");
 
-            Play();
-
-            Console.WriteLine("Thanks For Playing.");
-        }
-
-        static void Play()
-        {
-            string userInput;
-            string Computer;
             bool keepPlaying = true;
-            Random myRandomObject = new Random();
 
             try
             {
                 while (keepPlaying)
                 {
-
-                    Computer = "";
-                    userInput = "";
-
-                    while (userInput != "ROCK" && userInput != "PAPER" && userInput != "SCISSORS")
-                    {
-                        Console.WriteLine("Please enter your choice. ROCK, PAPER or SCISSORS");
-                        userInput = Console.ReadLine();
-                        userInput = userInput.ToUpper();
-                    }
-
-                    var randomNum = myRandomObject.Next(1, 4);
-                    switch (randomNum)
-                    {
-                        case 1:
-                            Computer = "ROCK";
-                            break;
-                        case 2:
-                            Computer = "PAPER";
-                            break;
-                        case 3:
-                            Computer = "SCISSORS";
-                            break;
-                    }
-
-                    Console.WriteLine("User : " + userInput);
-                    Console.WriteLine("Computer : " + Computer);
-
-                    switch (userInput)
-                    {
-                        case "ROCK":
-                            if (Computer == "ROCK")
-                            {
-                                Console.WriteLine("Its Tie!");
-                            }
-                            else if (Computer == "PAPER")
-                            {
-                                Console.WriteLine("You Lost!!");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You Won!!");
-                            }
-                            break;
-                        case "PAPER":
-                            if (Computer == "ROCK")
-                            {
-                                Console.WriteLine("You Won!!");
-                            }
-                            else if (Computer == "PAPER")
-                            {
-                                Console.WriteLine("Its a Tie!");
-                            }
-                            else
-                            {
-                                Console.WriteLine("You Lost!!");
-                            }
-                            break;
-                        case "SCISSORS":
-                            if (Computer == "ROCK")
-                            {
-                                Console.WriteLine("You Lost!!");
-                            }
-                            else if (Computer == "PAPER")
-                            {
-                                Console.WriteLine("You Won!!");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Its a Tie!");
-                            }
-                            break;
-                    }
-
+                    Game.playGame();
                     Console.WriteLine("Would you like to play again ? ( Y or YES for play again )");
                     var answer = Console.ReadLine();
                     answer = answer.ToUpper();
@@ -110,8 +31,9 @@
                     }
                 }
             }
-            catch (Exception ex)
-            { Console.WriteLine(ex.Message); }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            Console.WriteLine("Thanks For Playing.");
         }
     }
 }
